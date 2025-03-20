32f38a08591714f2a7e406d25b72b8269fc06af6
Loup Thevenin <loup@mac.home>
2025-03-20 09:17:38 +0100

    double iter

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 20:34:12 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/03/19 09:33:33 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/03/20 09:04:01 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 
 template <typename T>
 void iter(T *array, size_t length, void (*func)(const T &)) {
+  size_t i = 0;
+  while (i < length) {
+    func(array[i]);
+    i++;
+  }
+}
+
+template <typename T> void iter(T *array, size_t length, void (*func)(T &)) {
   size_t i = 0;
   while (i < length) {
     func(array[i]);
